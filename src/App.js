1713059bd901875ffc985b99a4bbb6146d6e2544
manor hagage <manorhagage@gmail.com>
2021-03-30 10:24:28 +0300

    Read and arange diamonds file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import './App.css';
 
 function App() 
 {
+	const [ diamonds, setDiamonds ] = useState();
+	const [ fields, setFields ] = useState();
+
+	// Read Diamonds file and arange data
+	useEffect( async () => 
+	{
+		const file = await fetch( './Diamonds.csv' )
+		.then( response => response.text() )
+		.then( text => { return text })
+
+		const data = csvToArray( file );
+		setFields( data.shift() );
+		setDiamonds( data );
+	},[])
+
+	// Convert string to 2D array
+	function csvToArray ( csv ) 
+	{
+		let rows = [];
+		rows = csv.split("\n");
+	
+		return rows.map( row => 
+		{
+			return row.split(",");
+		});
+	};
+
+	// Sum total diamonds price
+	function totalPrice()
+	{
+		let totalPrice = 0;
+		diamonds.map( diamond => 
+		{
+			totalPrice += parseFloat( diamond[ diamond.length - 1 ] );
+		});
+		return totalPrice;
+	}
+	
 	return (
 		<div className="App">
 		
